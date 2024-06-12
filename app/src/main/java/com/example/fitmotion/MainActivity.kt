ad62720c685badf182a7b2fa79b1a3d93e32ee4f
Muhammad Saffa <sapaasapa8@gmail.com>
2024-06-12 20:56:10 +0700

    finish activity welcome

--- app/src/main/java/com/example/fitmotion/MainActivity.kt
+++ app/src/main/java/com/example/fitmotion/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.fitmotion
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,17 +8,17 @@ import androidx.core.splashscreen.SplashScreen
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.fitmotion.databinding.ActivityMainBinding
+import com.example.fitmotion.databinding.ActivityWelcomeBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWelcomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_welcome)
+
     }
 }
