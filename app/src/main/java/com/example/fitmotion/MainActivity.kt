d7e5e53246ce868a370533070bb2fb0ac2eae740
Muhammad Saffa <sapaasapa8@gmail.com>
2024-06-13 19:12:22 +0700

    finish goals UI

--- app/src/main/java/com/example/fitmotion/MainActivity.kt
+++ app/src/main/java/com/example/fitmotion/MainActivity.kt
@@ -8,7 +8,9 @@ import androidx.core.splashscreen.SplashScreen
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.fitmotion.databinding.ActivityGoalsBinding
 import com.example.fitmotion.databinding.ActivityMainBinding
+import com.example.fitmotion.databinding.ActivityScreeningBinding
 import com.example.fitmotion.databinding.ActivityWelcomeBinding
 
 class MainActivity : AppCompatActivity() {
--- app/src/main/java/com/example/fitmotion/ScreeningActivity.kt
+++ app/src/main/java/com/example/fitmotion/ScreeningActivity.kt
@@ -1,12 +1,17 @@
 package com.example.fitmotion
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import com.example.fitmotion.databinding.ActivityScreeningBinding
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class ScreeningActivity : AppCompatActivity() {
     private lateinit var binding: ActivityScreeningBinding
+    private val calendar: Calendar = Calendar.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +22,27 @@ class ScreeningActivity : AppCompatActivity() {
         binding.illnessAutoComplete.setAdapter(arrayAdapter)
         setContentView(binding.root)
 
+        val date = DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+            calendar.set(Calendar.YEAR, year)
+            calendar.set(Calendar.MONTH, month)
+            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateLabel()
+        }
+
+        binding.dateEditText.setOnClickListener {
+            DatePickerDialog(this,
+                date,
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)
+            ).show()
+        }
+
+    }
+
+    private fun updateLabel() {
+        val myFormat = "dd/MM/yy" // Ubah format menjadi dd/MM/yy
+        val dateFormat = SimpleDateFormat(myFormat, Locale.US)
+        binding.dateEditText.setText(dateFormat.format(calendar.time))
     }
 }
